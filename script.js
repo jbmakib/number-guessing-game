0408926fa25b096bb6e22c94fe44f6ebf0dc978d
jbmakib <mdfoujolmobin@gmail.com>
2021-08-10 09:13:51 +0600

    updating loop

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-    let numOfPer = 0;
+let numOfPer = 0;
 
-    const randomNum = Math.floor(Math.random() * 10) + 1;
-    let promptMsg = "\nEnter a number from 1 to 10"
-    for (let i = 0; i < 5; i++) {
-        let givenNum = parseInt(prompt(promptMsg));
-        if (givenNum == randomNum) {
-            promptMsg = "Perfect Number\nEnter a number from 1 to 10";
-            numOfPer++;
-        } else if (givenNum < randomNum) {
-            promptMsg = "Lower Number\nEnter a number from 1 to 10";
-        } else if (givenNum > randomNum) {
-            promptMsg = "Higher Number\nEnter a number from 1 to 10";
-        };
+const randomNum = Math.floor(Math.random() * 10) + 1;
+let promptMsg = "\nEnter a number from 1 to 10"
+for (let i = 0; i < 5; i++) {
+    let givenNum = parseInt(prompt(promptMsg));
+    if (givenNum == randomNum) {
+        promptMsg = "Perfect Number\nEnter a number from 1 to 10";
+        numOfPer++;
+    } else if (givenNum < randomNum) {
+        promptMsg = "Lower Number\nEnter a number from 1 to 10";
+    } else if (givenNum > randomNum) {
+        promptMsg = "Higher Number\nEnter a number from 1 to 10";
     };
-    numOfPer = numOfPer * 20;
-    document.querySelector("#display").innerHTML = ("Your score is: " + numOfPer);
+};
+numOfPer = numOfPer * 20;
+document.querySelector("#display").innerHTML = ("Your score is: " + numOfPer);
